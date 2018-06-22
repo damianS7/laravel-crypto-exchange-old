a9a9f989ae0c5071f2a6bd378c26e61ee506f0f0
damianS7 <josepwnz@gmail.com>
2018-06-23 00:55:04 +0200

    redirects are now temporary

--- routes/web.php
+++ routes/web.php
@@ -11,43 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-
-Route::get('/admin/', function () {
-    // if user mail josepwnz == admin
-    return view('user/login');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 // Exchange
-Route::redirect('/trade/', '/trade/BTC-USDT', 301);
+Route::get('/trade/changeTheme{theme?}', 'TradeController@changeTheme')->name('change-theme');
+Route::redirect('/trade/', '/trade/BTC-USDT', 302);
 Route::get('/trade/{pair?}', 'TradeController@index')->name('trade');
-Route::get('/trade/', 'TradeController@changeTheme')->name('change-theme');
-//Route::get('/trade/{pair?}', function ($pair) {
-    // Pasar el pair al controlador para su filtrado
-    // Pasar coins markets balances etc a la vista
-    //return view('exchange/trade', array('data' => $pair));
-
-//})->name('trade');
-//->where('name', '[A-Za-z]+')
 
 // Admin routes
 Route::get('/admin/', 'Admin\SettingsController@index')->name('admin');
-Route::redirect('/admin/', '/admin/settings/', 301);
+Route::redirect('/admin/', '/admin/settings/', 302);
 Route::get('/admin/accounts', 'Admin\AccountsController@index')->name('admin-accounts');
 Route::get('/admin/settings/', 'Admin\SettingsController@index')->name('admin-settings');
 Route::post('/admin/settings/', 'Admin\SettingsController@save')->name('admin-save-settings');
 Route::get('/admin/coins', 'Admin\CoinsController@index')->name('admin-coins');
-
 Route::get('/admin/pairs', 'Admin\PairsController@index')->name('admin-pairs');
 Route::get('/admin/markets', 'Admin\MarketsController@index')->name('admin-markets');
 Route::get('/admin/withdrawals', 'Admin\WithdrawalsController@index')->name('admin-withdrawals');
 
 // Account routes
 Route::get('/account/', 'Account\OverviewController@index')->name('account');
-Route::redirect('/account/', '/account/overview/', 301);
+Route::redirect('/account/', '/account/overview/', 302);
 Route::get('/account/overview/', 'Account\OverviewController@index')->name('overview');
 Route::get('/account/balances/', 'Account\BalancesController@index')->name('balances');
 Route::get('/account/deposits/', 'Account\DepositsController@index')->name('deposits');
@@ -58,7 +42,7 @@ Route::get('/account/security/', 'Account\SecurityController@index')->name('secu
 Route::get('/account/tradehistory/', 'Account\TradeHistoryController@index')->name('tradehistory');
 
 Route::post('/account/deposits/deposit', 'Account\DepositsController@deposit')->name('deposit');
-Route::redirect('/account/deposits/deposit', '/account/deposits/', 301);
+Route::redirect('/account/deposits/deposit', '/account/deposits/', 302);
 
 // Auth routes
 Auth::routes();
