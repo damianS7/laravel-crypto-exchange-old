9001ba8e2d6985939a3e6ef720c8d209d8e03015
damianS7 <josepwnz@gmail.com>
2018-06-24 22:19:28 +0200

    now adding new coins and market are avaliable

--- app/Http/Controllers/Admin/CoinsController.php
+++ app/Http/Controllers/Admin/CoinsController.php
@@ -9,18 +9,126 @@ use App\Http\Models\Coin;
 class CoinsController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    * Create a new controller instance.
+    *
+    * @return void
+    */
     public function __construct()
     {
         $this->middleware('auth-admin');
     }
 
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function index()
     {
         $coins = Coin::paginate(5);
-        return view('admin/coins')->with('coins', $coins);
+        return view('admin/coins.index')->with(compact('coins'));
     }
+
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+        return view('admin/coins.create');
+    }
+
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Http\Models\Coin  $coin
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        try {
+            $coin = new Coin;
+            $coin->symbol = $request->input('symbol');
+            $coin->name = $request->input('name');
+            $coin->min_deposit = $request->input('min_deposit');
+            $coin->min_withdrawal = $request->input('min_withdrawal');
+            $coin->fee_withdrawal = $request->input('fee_withdrawal');
+            $coin->save();
+            \Session::flash('success', 'New coin created');
+        } catch (\Illuminate\Database\QueryException $e) {
+            \Session::flash('error', 'Coin not added');
+        }
+        return redirect ('admin/coins/');
+    }
+
+    /**
+    * Display the specified resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function show($coinId)
+    {
+        $coin = Coin::where('id', $coinId)->firstOrFail();
+        return view('admin/coins.show')->with(compact('coin'));
+    }
+
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function edit($coinId)
+    {
+        $coin = Coin::where('id', $coinId)->firstOrFail();
+        return view('admin/coins.edit')->with(compact('coin'));
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Http\Models\Coin  $coin
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, Coin $coin)
+    {
+        if( $request->submit == 'update' ) {
+            $coin->symbol = $request->input('symbol');
+            $coin->name = $request->input('name');
+            $coin->min_deposit = $request->input('min_deposit');
+            $coin->min_withdrawal = $request->input('min_withdrawal');
+            $coin->fee_withdrawal = $request->input('fee_withdrawal');
+
+            try {
+                $coin->save();
+                \Session::flash('success', 'Coin updated.');
+            } catch (\Illuminate\Database\QueryException $e) {
+                \Session::flash('error', 'Coin cannot be updated.');
+            }
+        }
+
+        return redirect ('admin/coins/' . $coin->id . '/edit');
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\Http\Models\Coin  $coin
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy(Request $request, Coin $coin)
+    {
+        if( $request->submit == 'delete' ) {
+            try {
+                $coin->delete();
+                \Session::flash('success', 'New coin added.');
+            } catch (\Illuminate\Database\QueryException $e) {
+                \Session::flash('error', 'Coin has been deleted.');
+            }
+        }
+        return redirect ('admin/coins');
+    }
+
 }
--- app/Http/Controllers/Admin/MarketsController.php
+++ app/Http/Controllers/Admin/MarketsController.php
@@ -2,25 +2,142 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Models\Market;
+use App\Http\Models\Coin;
+use Collective\Html\Form;
 
 class MarketsController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    * Create a new controller instance.
+    *
+    * @return void
+    */
     public function __construct()
     {
         $this->middleware('auth-admin');
     }
 
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function index()
     {
-        $markets = Market::paginate(5);
-        return view('admin/markets')->with('markets', $markets);
+        $markets = Market::select('coins.symbol', 'markets.*')
+			->join('coins', 'markets.coin_id', '=', 'coins.id')
+			->paginate(5);
+        return view('admin/markets.index')->with(compact('markets'));
+    }
+
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+        //$coins = Coin::all();
+        $coins = DB::select('SELECT * FROM coins c WHERE NOT EXISTS (SELECT m.coin_id FROM markets m WHERE c.id = m.coin_id)');
+        return view('admin/markets.create')->with(compact('coins'));
+    }
+
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Http\Models\Coin  $coin
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        try {
+            $coin = Coin::findOrFail($request->input('coin_id'));
+            $market = new Market;
+            $market->coin_id = $coin->id;
+            $market->save();
+            \Session::flash('success', 'New market created');
+        } catch (\Illuminate\Database\QueryException $e) {
+            \Session::flash('error', 'Market not added');
+        }
+        return redirect ('admin/markets/');
+    }
+
+    /**
+    * Display the specified resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function show($marketId)
+    {
+        $market = Market::select('coins.symbol', 'markets.*')
+			->join('coins', 'markets.coin_id', '=', 'coins.id')
+            ->where('markets.id', $marketId)->firstOrFail();
+
+        return view('admin/markets.show')->with(compact('market'));
+    }
+
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function edit($marketId)
+    {
+        $market = Market::select('coins.symbol', 'markets.*')
+			->join('coins', 'markets.coin_id', '=', 'coins.id')
+            ->where('markets.id', $marketId)->firstOrFail();
+
+        return view('admin/markets.edit')->with(compact('market'));
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Http\Models\Market  $market
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, Market $market)
+    {
+        if( $request->submit == 'open_market' ) {
+            $market->market_open = 1;
+        }
+
+        if( $request->submit == 'close_market' ) {
+            $market->market_open = 0;
+        }
+
+        try {
+            $market->save();
+            \Session::flash('success', 'Market updated.');
+        } catch (\Illuminate\Database\QueryException $e) {
+           \Session::flash('error', 'Market cannot be updated.');
+        }
+
+        return redirect ('admin/markets/' . $market->id . '/edit');
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\Http\Models\Market  $market
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy(Request $request, Market $market)
+    {
+        if( $request->submit == 'delete' ) {
+            try {
+                $market->delete();
+                \Session::flash('success', 'New market added.');
+            } catch (\Illuminate\Database\QueryException $e) {
+                \Session::flash('success', 'Market has been deleted.');
+            }
+        }
+        return redirect ('admin/markets');
     }
 }
