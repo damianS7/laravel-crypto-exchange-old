ade117dcf69c5bcad9035e12b5b3fd67ae606ba2
damianS7 <josepwnz@gmail.com>
2018-06-30 15:53:20 +0200

    order form fixes and id, classes names changed

--- resources/views/exchange/orderForm.blade.php
+++ resources/views/exchange/orderForm.blade.php
@@ -1,87 +1,103 @@
 <div class="row">
+	<div class="col-sm-6">
+		<div class="form-group">
 
-<div class="col-sm-6">
-	<div class="form-group">
-    <label class="control-label">
-		<?php //echo "Buy: " . $pair[1] . " Your Balance: " . $_SESSION['balances'][$pair[1]] . " " . $pair[1]; ?>
-	</label>
-		<div class="form-group row">
-				<label for="" class="col-sm-12 col-form-label">
-				@if(Auth::check())
-				<small>Balance:{{ $balance[$coin1->symbol] }} {{ $coin1->symbol }}</small>
-				@endif
-				</label>
+			<div class="form-group row">
+				<div class="col-sm-6">
+					<small>Buy {{ $coin1->symbol }}</small>
+				</div>
+				<div class="col-sm-6 text-right">
+					<small>
+						@if(Auth::check())
+							<span id="buy-balance" class="balance clickable">{{ $balance[$coin2->symbol] }}</span> {{ $coin2->symbol}}
+						@else
+							<span>-</span> {{ $coin2->symbol}}
+						@endif
+					</small>
+				</div>
 			</div>
-		<div class="form-group row">
-			<label for="staticPrice" class="col-sm-2 col-form-label">Price</label>
-			<div class="col-sm-10 input-group mb-3">
-				<input class="form-control" id="input-buy-price" type="text" value="0.0005">
 
-				<div class="input-group-append">
-					<span class="input-group-text">USDT</span>
+			<div class="form-group row">
+				<label for="" class="col-sm-3 col-form-label">Price</label>
+				<div class="col-sm-9 input-group mb-3">
+					<input class="form-control" id="input-buy-price" type="text" value="">
+					<div class="input-group-append">
+						<span class="input-group-text">{{ $coin2->symbol }}</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label for="" class="col-sm-3 col-form-label">Amount</label>
+				<div class="col-sm-9 input-group mb-3">
+					<input class="form-control" id="input-buy-amount" type="text" value="">
+					<div class="input-group-append">
+						<span class="input-group-text">{{ $coin1->symbol }}</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<div class="col-sm-3">
+					<span class="">Total</span>
+				</div>
+				<div class="col-sm-9 text-right">
+					<span id="total-buy" class="">-</span> {{ $coin2->symbol }}
 				</div>
 			</div>
 		</div>
-
-		<div class="form-group row">
-			<label for="staticEmail" class="col-sm-2 col-form-label">Amount</label>
-			<div class="col-sm-10 input-group mb-3">
-				<input class="form-control" id="input-buy-amount" type="text" value="0.0005">
-				<div class="input-group-append">
-					<span class="input-group-text">BTC</span>
-				</div>
-			</div>
-		</div>
-
-		<div class="form-group row">
-			<label for="staticEmail" class="col-sm-2 col-form-label">Total</label>
-			<div class="col-sm-10 input-group mb-3">
-				<span name="total-buy" class="">-</span> <?php //echo $pair[1]; ?>
-			</div>
-		</div>
+		<button type="submit" id="btn-buy" data-type="buy" data-pair="{{ $pair->id }}" class="btn btn-success btn-block">BUY</button>
+		<p>Setting buyfee {{ @$settings['buy_fee']->value }}</p>
 	</div>
-	<button type="submit" id="btn-buy" data-type="buy" data-pair="{{ $pair->id }}" class="btn btn-success btn-block">BUY</button>
-	<p>Setting buyfee {{ @$settings['buy_fee']->value }}</p>
-</div>
 
-<div class="col-sm-6">
-	<div class="form-group">
-		<label class="control-label">
-			<?php //echo "Buy: " . $pair[0] . " Your Balance: " . $_SESSION['balances'][$pair[0]] . " " . $pair[0]; ?>
-		</label>
-		<div class="form-group row">
-			<label for="" class="col-sm-12 col-form-label">
-				@if(Auth::check())
-				<small>Balance:{{ $balance[$coin2->symbol] }} {{ $coin2->symbol }}</small>
-				@endif
-			</label>
-		</div>
-		<div class="form-group row">
-			<label for="staticEmail" class="col-sm-2 col-form-label">Price</label>
-			<div class="col-sm-10 input-group mb-3">
-				<input class="form-control" id="input-sell-price" value="0.0005" type="text">
-				<div class="input-group-append">
-					<span class="input-group-text">USDT</span>
+	<div class="col-sm-6">
+		<div class="form-group">
+
+			<div class="form-group row">
+				<div class="col-sm-6">
+					<small>Sell {{ $coin1->symbol }}</small>
+				</div>
+				<div class="col-sm-6 text-right">
+					<small>
+						@if(Auth::check())
+							<span id="sell-balance" class="balance clickable">{{ $balance[$coin1->symbol] }}</span> {{ $coin1->symbol}}
+						@else
+							<span>-</span> {{ $coin1->symbol}}
+						@endif
+					</small>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label for="" class="col-sm-3 col-form-label">Price</label>
+				<div class="col-sm-9 input-group mb-3">
+					<input class="form-control" id="input-sell-price" type="text" value="">
+					<div class="input-group-append">
+						<span class="input-group-text">{{ $coin2->symbol }}</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label for="" class="col-sm-3 col-form-label">Amount</label>
+				<div class="col-sm-9 input-group mb-3">
+					<input class="form-control" id="input-sell-amount" type="text" value="">
+					<div class="input-group-append">
+						<span class="input-group-text">{{ $coin1->symbol }}</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<div class="col-sm-3">
+					<span class="">Total</span>
+				</div>
+				<div class="col-sm-9 text-right">
+					<span id="total-sell" class="">-</span> {{ $coin2->symbol }}
 				</div>
 			</div>
 		</div>
-		<div class="form-group row">
-			<label for="staticEmail" class="col-sm-2 col-form-label">Amount</label>
-			<div class="col-sm-10 input-group mb-3">
-				<input class="form-control" id="input-sell-amount" value="0.0005" type="text">
-				<div class="input-group-append">
-					<span class="input-group-text">BTC</span>
-				</div>
-			</div>
-		</div>
-
-		<div class="form-group row">
-			<label for="staticEmail" class="col-sm-2 col-form-label">Total</label>
-			<div class="col-sm-10 input-group mb-3">
-				<span name="total-sell" class="">-</span> <?php //echo $pair[1]; ?>
-			</div>
-		</div>
+		<button type="submit" id="btn-sell" data-type="sell" data-pair="{{ $pair->id }}" class="btn btn-danger btn-block">SELL</button>
+		<p>Setting sellee {{ @$settings['sell_fee']->value }}</p>
 	</div>
-	<button type="submit" id="btn-sell" data-type="sell" data-pair="{{ $pair->id }}" class="btn btn-danger btn-block">SELL</button>
-</div>
 </div>
